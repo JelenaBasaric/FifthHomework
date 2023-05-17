721f25fcaf2a2f18675241d3624aef15e843298b
Jelena Basaric <jbasaric83@gmail.com>
2023-05-17 10:54:29 +0200

    all

--- src/app/all-users/all-users.component.ts
+++ src/app/all-users/all-users.component.ts
@@ -31,7 +31,7 @@ export class AllUsersComponent {
       //callbackFunk;
      };
   
-  var runIt(function (){
-  => callbackFuncRunIt();)
- }
+//   var runIt(function (){
+//   => callbackFuncRunIt();)
+//  }
     }
